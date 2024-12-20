a22873d7e0b264ef8feaa4a9fc1f70ea7e5754f9
brian <muriithibrian283@gmail.com>
2024-12-20 16:26:36 +0300

    Fix coding style for file 8 and 10

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.esrever = function(list) {
-  let len = list.length -1;
+exports.esrever = function (list) {
+  let len = list.length - 1;
   for (let i = 0; (len - i) > 0; i++) {
     const x = list[len];
     list[len] = list[i];
